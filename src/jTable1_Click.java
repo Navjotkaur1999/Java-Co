a93c4774b148c07dd897297f576e0cf0300af197
Navjot <jotnav1999@gmail.com>
2020-04-06 20:04:35 +0530

    This is a Course outcome attainment project

---
+++ src/jTable1_Click.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Navjot
+ */
+class jTable1_Click {
+
+    static int getSelectedColumn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/jTable_Click.java
@@ -0,0 +1,20 @@
+
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Navjot
+ */
+class jTable_Click {
+
+    static TableModel getModel() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
